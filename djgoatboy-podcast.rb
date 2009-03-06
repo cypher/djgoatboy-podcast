4cbdde0b4b8b8c50b4db9ab9413db4959568c47c
Markus Prinz <markus.prinz@nuclearsquid.com>
2009-03-06 18:25:47 +0100

    Update builder code to write directly to file

--- djgoatboy-podcast.rb
+++ djgoatboy-podcast.rb
@@ -30,48 +30,46 @@ DJGOATBOY_FEED = 'http://twitter.com/statuses/user_timeline/17939483.atom'
 PODCAST_FILE = File.expand_path("~/www/nuclearsquid.com/djgoatboy.rss")
 
 begin
-  feed = SimpleRSS.parse( open(DJGOATBOY_FEED) )
+  feed = SimpleRSS.parse( open(DJGOATBOY_FEED) ) # rescue SocketError
 
-  builder = Builder::XmlMarkup.new
-  builder.instruct!
-  # There's gotta be a better way than this
-  podcast = builder.rss("version" => "2.0") { |rss|
-    rss.channel { |channel|
-      channel.title "@djgoatboy's daily mp3 feed"
-      channel.link feed.link
-      channel.language 'en-US'
+  File.open(PODCAST_FILE, 'w+') do |file|
+    builder = Builder::XmlMarkup.new(:target => file, :indent => 4)
+    builder.instruct!
+    # There's gotta be a better way than this
+    podcast = builder.rss("version" => "2.0") { |rss|
+      rss.channel { |channel|
+        channel.title "@djgoatboy's daily mp3 feed"
+        channel.link feed.link
+        channel.language 'en-US'
 
-      feed.entries.each do |entry|
-        if entry.content =~ %r{(http://tinyurl\.com/[a-zA-Z0-9]+)}
-          url = untinyurl(entry.content)
+        feed.entries.each do |entry|
+          if entry.content =~ %r{(http://tinyurl\.com/[a-zA-Z0-9]+)}
+            url = untinyurl(entry.content)
 
-          # Make sure we're getting a mp3
-          if url =~ /\.mp3$/
-            length = mp3_length(url)
+            # Make sure we're getting a mp3
+            if url =~ /\.mp3$/
+              length = mp3_length(url)
 
-            channel.item {|item|
-              item.title "@djgoatboy / #{Time.now.strftime("%d-%m-%Y")}"
-              item.link entry.link
+              channel.item {|item|
+                item.title "@djgoatboy / #{Time.now.strftime("%d-%m-%Y")}"
+                item.link entry.link
 
-              description = entry.content.gsub(%r{\s*http://tinyurl\.com/[a-zA-Z0-9]+\s*}, '').gsub(%r{djgoatboy:\s*}, '')
-              if description.empty?
-                item.description "(no description)"
-              else
-                item.description description
-              end
+                description = entry.content.gsub(%r{\s*http://tinyurl\.com/[a-zA-Z0-9]+\s*}, '').gsub(%r{djgoatboy:\s*}, '')
+                if description.empty?
+                  item.description "(no description)"
+                else
+                  item.description description
+                end
 
-              # Right now, we'll trust giles to only release mp3s
-              item.enclosure("url" => url, "length" => length, "type" => "audio/mpeg")
-              item.pubDate entry.published
-            }
+                # Right now, we'll trust giles to only release mp3s
+                item.enclosure("url" => url, "length" => length, "type" => "audio/mpeg")
+                item.pubDate entry.published
+              }
+            end
           end
         end
-      end
+      }
     }
-  }
-
-  File.open(PODCAST_FILE, 'w+') do |f|
-    f.write( podcast )
   end
 
   File.open(TINYURLS_YAML, 'w+') do |f|
